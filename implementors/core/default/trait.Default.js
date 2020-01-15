4b9338f7409409da7218fa8bd375d296c07cae98
HiruNya <HiruNya@users.noreply.github.com>
2020-01-15 14:51:13 +0000

    Automated deployment: Wed Jan 15 14:51:13 UTC 2020 78ffde9d44c3603f88ad1de30421ea44480f91af

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["dawn_wait"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"dawn_wait/struct.Listener.html\" title=\"struct dawn_wait::Listener\">Listener</a>",synthetic:false,types:["dawn_wait::listener::Listener"]},];
+implementors["dawn_wait"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"dawn_wait/struct.Listener.html\" title=\"struct dawn_wait::Listener\">Listener</a>","synthetic":false,"types":["dawn_wait::listener::Listener"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/futures_core/stream/trait.Stream.js
+++ implementors/futures_core/stream/trait.Stream.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["dawn_wait"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/futures-core/0.3.0/futures_core/stream/trait.Stream.html\" title=\"trait futures_core::stream::Stream\">Stream</a> for <a class=\"struct\" href=\"dawn_wait/struct.WaitForMultiple.html\" title=\"struct dawn_wait::WaitForMultiple\">WaitForMultiple</a>",synthetic:false,types:["dawn_wait::wait::WaitForMultiple"]},];
+implementors["dawn_wait"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/futures-core/0.3.0/futures_core/stream/trait.Stream.html\" title=\"trait futures_core::stream::Stream\">Stream</a> for <a class=\"struct\" href=\"dawn_wait/struct.WaitForMultiple.html\" title=\"struct dawn_wait::WaitForMultiple\">WaitForMultiple</a>","synthetic":false,"types":["dawn_wait::wait::WaitForMultiple"]}];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
