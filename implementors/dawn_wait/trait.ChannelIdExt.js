251f31d8665c9793fb4f2b14a92ecea53a5ace2f
HiruNya <HiruNya@users.noreply.github.com>
2020-01-16 07:27:43 +0000

    Automated deployment: Thu Jan 16 07:27:43 UTC 2020 11703a09144d5fff569cd7624e129bf2102b93ee

---
+++ implementors/dawn_wait/trait.ChannelIdExt.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["dawn_wait"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
